45a830b6f598d8c9ce883dafa4cf26efd7dbb3e0
John Cordano <jecordano@yahoo.com>
2018-02-12 22:22:41 -0800

    Added API keys to files.

--- liri.js
+++ liri.js
@@ -1,1 +1,35 @@
 require("dotenv").config();
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+switch (action) {
+  case "my-tweets":
+    myTweets();
+    break;
+  case "spotify-this-song":
+    spotifyThisSong();
+    break;
+  case "movie-this":
+    movieThis();
+    break;
+  case "do-what-it-says":
+    doWhatItSays();
+    break;
+}
+
+function myTweets() {
+
+}
+
+function spotifyThisSong() {
+
+}
+
+function movieThis() {
+
+}
+
+function doWhatIsSays() {
+	
+}
