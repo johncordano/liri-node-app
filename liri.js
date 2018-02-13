5ab3366741aad1671d5cf742b991b22227b35d7d
John Cordano <jecordano@yahoo.com>
2018-02-13 15:15:23 -0800

    Added require variables for third-party APIs.

--- liri.js
+++ liri.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var request = require('request');
 var fs = require("fs");
 
 var spotify = new Spotify(keys.spotify);
